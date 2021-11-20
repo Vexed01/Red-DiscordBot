25ffb36b43c2c3ab6d4b9396c551222d5bd26046
Vexed01 <vex@vexcodes.com>
2021-11-20 17:17:38 +0000

    black!

--- redbot/cogs/downloader/downloader.py
+++ redbot/cogs/downloader/downloader.py
@@ -6,8 +6,7 @@ import shutil
 import sys
 from collections import defaultdict
 from pathlib import Path
-from typing import (Collection, Dict, Iterable, List, Optional, Set, Tuple,
-                    Union, cast)
+from typing import Collection, Dict, Iterable, List, Optional, Set, Tuple, Union, cast
 
 import discord
 from redbot.core import Config, checks, commands
@@ -15,8 +14,7 @@ from redbot.core import version_info as red_version_info
 from redbot.core.bot import Red
 from redbot.core.data_manager import cog_data_path
 from redbot.core.i18n import Translator, cog_i18n
-from redbot.core.utils.chat_formatting import (box, humanize_list, inline,
-                                               pagify)
+from redbot.core.utils.chat_formatting import box, humanize_list, inline, pagify
 from redbot.core.utils.menus import start_adding_reactions
 from redbot.core.utils.predicates import MessagePredicate, ReactionPredicate
 
